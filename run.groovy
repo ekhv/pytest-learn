5cac977f1d40bb6aaf077e11ba7d4410da669269
evgkv <e.khryashchev@outlook.com>
2020-11-03 00:12:17 +0500

    rm stage

--- run.groovy
+++ run.groovy
@@ -1,11 +1,6 @@
 pipeline {
     agent any
     stages {
-        stage("Env Variables") {
-            steps {
-                sh "printenv"
-            }
-        }
         stage('Test') {
             steps {
                 sh 'python3 -m pytest --junit-xml results.xml test_with_pytest.py'
