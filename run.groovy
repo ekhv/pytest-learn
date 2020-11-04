d2b4f216a56e6e30b1dd00de6a9aabb9250ccf61
evgkv <e.khryashchev@outlook.com>
2020-11-04 16:07:17 +0500

    upd job

--- run.groovy
+++ run.groovy
@@ -1,16 +1,50 @@
+import hudson.AbortException
+
 pipeline {
     agent any
+    parameters {
+        string(defaultValue: '', description: 'app server', name: 'app_server')
+        password(defaultValue: '', description: 'Passphrase private key', name: 'pass_private_key')
+    }
+
+    environment {
+        app_server = "$app_server"
+        passphrase = "$pass_private_key"
+    }
+
     stages {
+        stage('Checks') {
+            steps {
+                script {
+                    if (pass_private_key.size() == 0) {
+                        currentBuild.result = "FAILURE"
+                        throw new AbortException("Error. pass_private_key  is empty.")
+                    }
+
+                    if (app_server.isEmpty()) {
+                        currentBuild.result = "FAILURE"
+                        throw new AbortException("Error. app_server is empty.")
+                    }
+                }
+            }
+        }
+
+        stage('Info') {
+            steps {
+                println  "app server $app_server"
+            }
+        }
+
         stage('Test') {
             steps {
                 sh 'python3 -m pytest --junit-xml results.xml test_with_pytest.py'
             }
-            post {
-                always {
-                    println "Test result\n${env.RUN_TESTS_DISPLAY_URL}"
-                    junit 'results.xml'
-                }
-            }
+        }
+    }
+    post {
+        always {
+            println "Test result\n${env.RUN_TESTS_DISPLAY_URL}"
+            junit 'results.xml'
         }
     }
 }
